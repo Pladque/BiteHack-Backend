ae08b604adb9dbc038b4590cf6883693040c167f
Pladque <63751303+Pladque@users.noreply.github.com>
2021-01-16 16:49:58 +0100

    superuser have permission to everything

--- Snowledge/polls/models.py
+++ Snowledge/polls/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
-
 class Tag(models.Model):
     content = models.CharField(max_length=50, null=False)
 
     def __str__(self):
         return '{}'.format(self.content)
-        
+
 class Members(models.Model):
     role = models.CharField(max_length=50, null=False)
     django_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='django_user', null = False, blank=True)
@@ -17,7 +16,7 @@ class Members(models.Model):
 class Question(models.Model):
     title = models.CharField(max_length=50, null=False)
     content = models.CharField(max_length=3000, null=False)
-    tags = models.ManyToManyField(Tag, null = False)
+    tags = models.ManyToManyField(Tag)
     date_created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
