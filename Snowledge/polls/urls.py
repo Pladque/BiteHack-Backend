2337bed0cd8361581fa9e7cfd5b7079c927de14b
skyman503 <karolsygiet@gmail.com>
2021-01-16 22:11:23 +0100

    own questions

--- Snowledge/polls/urls.py
+++ Snowledge/polls/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('add_problem/', add_problem, name='add_problem'),
     path('profile/', user_skills, name='user_skills'),
+    path('my_questions/', MyQuestions, name='MyQuestions'),
     path('question/<pk>', QuestionDetailView, name='get_question'),
 ]
