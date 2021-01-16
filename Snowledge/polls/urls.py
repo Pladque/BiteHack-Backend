4da93988ecc486cc149c1e6677995ef8224057dd
Pladque <63751303+Pladque@users.noreply.github.com>
2021-01-16 22:07:28 +0100

    delete and solved urls

--- Snowledge/polls/urls.py
+++ Snowledge/polls/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('add_problem/', add_problem, name='add_problem'),
     path('profile/', user_skills, name='user_skills'),
     path('question/<pk>', QuestionDetailView, name='get_question'),
+
+    path('question/<pk>', delete_question, name='delete_question'),
+    path('question/<pk>', mark_as_solved, name='mark_as_solved')
 ]
