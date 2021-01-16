bc3bddc18d25d03afcc1ef0bc663f9ff985e21e7
Pladque <63751303+Pladque@users.noreply.github.com>
2021-01-16 15:46:47 +0100

    quesitions, tags and answers
    basics

--- Snowledge/polls/models.py
+++ Snowledge/polls/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    content = models.CharField(max_length=3000, null=False)
+    tags = models.ManyToManyField(Tag)
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+    class Meta:
+        ordering = ('-date_created',)
+
+class Tag(models.Model):
+    content = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return '{}'.format(self.content)
+
+class Answer(models.Model):
+    owner = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='my_question', null = True, blank=True)
+    likes = models.IntegerField(default = 0, null=True, blank = True)
+    date_created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('likes',)   #not sure if it work that way xd 
+
