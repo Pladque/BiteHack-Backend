3ae7a95998e98810659a0cbefac78670a03a313b
Pladque <63751303+Pladque@users.noreply.github.com>
2021-01-16 18:47:09 +0100

    register work

--- Snowledge/users/urls.py
+++ Snowledge/users/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views as users_views
 from django.contrib.auth import views as auth_views
+from .forms import *
 
 urlpatterns = [
     path('register/', users_views.register, name='register'),
--- Snowledge/users/views.py
+++ Snowledge/users/views.py
@@ -9,4 +9,4 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
