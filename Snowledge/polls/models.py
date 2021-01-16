deecd3b30eced6c329799425002cfe79ad24d81e
Pladque <63751303+Pladque@users.noreply.github.com>
2021-01-16 16:42:58 +0100

    class members

--- Snowledge/polls/models.py
+++ Snowledge/polls/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
+
+class Members(models.Model):
+    role = models.CharField(max_length=50, null=False)
+    django_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='django_user', null = False, blank=True)
+
 class Question(models.Model):
     title = models.CharField(max_length=50, null=False)
     content = models.CharField(max_length=3000, null=False)
