524a3a1d10e953ec7546b6442c26820d851908e5
Pladque <63751303+Pladque@users.noreply.github.com>
2021-01-17 10:10:52 +0100

    zwraca tagi

--- Snowledge/polls/views.py
+++ Snowledge/polls/views.py
@@ -16,18 +16,23 @@ def index(request):
 
     questions_for_me = []
     questions_not_for_me = []
+    tags_for_me = []
+    tags_not_for_me = []
     found = False
     for question in q:
         for tag in question.tags.all():
             if tag in my_skills:
                 questions_for_me.append(question)
                 found = True
+                tags_for_me.append(question.tags.all())
                 break
         if not found:
+            tags_not_for_me.append(question.tags.all())
             questions_not_for_me.append(question)
 
+    
 
-    return render(request, 'polls/homepage.html', {'questions_for_me': questions_for_me, 'questions_not_for_me':questions_not_for_me})
+    return render(request, 'polls/homepage.html', {'questions_for_me': questions_for_me, 'questions_not_for_me':questions_not_for_me, 'tags_for_me':tags_for_me,'tags_not_for_me':tags_not_for_me})
 
 @login_required
 def add_problem(request):
